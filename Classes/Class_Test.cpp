8b16fc45d73710633f888580f3f7b6fd8948c0a4
Mahmoud Omara <111393894+Mahmoud21030@users.noreply.github.com>
2024-04-11 23:55:34 +0000

    renamed:    Test -> Classes/Class_Test 	renamed:    Class_Test.cpp -> Classes/Class_Test.cpp 	deleted:    tempCodeRunnerFile.cpp 	deleted:    test

--- Classes/Class_Test.cpp
+++ Classes/Class_Test.cpp
@@ -8,7 +8,7 @@ class myclass
     int T2;
     static int T3 ; /* Static members are only declared in a class declaration,
      not defined. They must be explicitly defined outside the class using the scope resolution operator.*/
-     
+
     //myclass() = default; //default constructot, Not user-provided
     myclass() {} //user-provided
 
@@ -17,12 +17,21 @@ class myclass
 
 };
 
+int myclass::T3 = 3;
 
 int main()
 {
     myclass e;
-    std::cout<<"Hello world"<<" "<<myclass()::T1<<std::endl; //using temp class
-    std::cout<<"T2: "<<myclass().T3<<std::endl; //garbage if user provided or 0 if Not user-provided
+    std::cout<<"Hello world"<<" "<<myclass().T1<<std::endl; //using temp class
+    // std::cout<<"Hello world"<<" "<<myclass::T1<<std::endl; //must be static
+
+    std::cout<<"T2: "<<e.T2<<std::endl; //garbage if user provided or 0 if Not user-provided
+    std::cout<<"T3: "<<myclass::T3<<std::endl; 
+    //std::cout<<"T3: "<<e::T3<<std::endl;  //error, must be the class
+
+    
+
+
     return 0;
 }
 
